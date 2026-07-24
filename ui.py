a3a958f998e72d85da67b4bd83d4a34dfab1a6c8
Devendra Khanal <devendrakhanal006@gmail.com>
2026-07-24 19:42:19 +0545

    Update ui.py

--- ui.py
+++ ui.py
@@ -524,6 +524,50 @@ class MetricBar(QWidget):
         self._text  = "--"
         self.setFixedHeight(38)
         self.setMinimumWidth(80)
+        
+    def set_value(self, pct: float, text: str):
+        self._value = max(0.0, min(100.0, pct))
+        self._text  = text
+        self.update()
+
+    def paintEvent(self, _):
+        p = QPainter(self)
+        p.setRenderHint(QPainter.RenderHint.Antialiasing)
+        W, H = self.width(), self.height()
+
+        p.setBrush(QBrush(qcol(C.PANEL2)))
+        p.setPen(QPen(qcol(C.BORDER_A), 1))
+        p.drawRoundedRect(QRectF(1, 1, W - 2, H - 2), 4, 4)
+
+        bar_h   = 4
+        bar_y   = H - bar_h - 5
+        bar_w   = W - 12
+        bar_x   = 6
+        fill_w  = int(bar_w * self._value / 100)
+
+        p.setBrush(QBrush(qcol(C.BAR_BG)))
+        p.setPen(Qt.PenStyle.NoPen)
+        p.drawRoundedRect(QRectF(bar_x, bar_y, bar_w, bar_h), 2, 2)
+
+        if self._value > 85:
+            bar_col = qcol(C.RED)
+        elif self._value > 65:
+            bar_col = qcol(C.ACC)
+        else:
+            bar_col = qcol(self._color)
+
+        if fill_w > 0:
+            p.setBrush(QBrush(bar_col))
+            p.drawRoundedRect(QRectF(bar_x, bar_y, fill_w, bar_h), 2, 2)
+
+        p.setFont(QFont("Courier New", 7, QFont.Weight.Bold))
+        p.setPen(QPen(qcol(C.TEXT_DIM), 1))
+        p.drawText(QRectF(8, 5, 50, 14), Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter, self._label)
+
+        p.setFont(QFont("Courier New", 9, QFont.Weight.Bold))
+        p.setPen(QPen(bar_col if self._text != "--" else qcol(C.TEXT_DIM), 1))
+        p.drawText(QRectF(0, 4, W - 6, 16), Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter, self._text)
+
 
 
         
